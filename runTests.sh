6f77cb37e5e530c0d1b302504234212c7e74eed1
david-cattermole <cattermole91@gmail.com>
2017-07-12 00:36:48 +0930

    New utility headers.

--- runTests.sh
+++ runTests.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 ./cmake-build-debug/debugUtils
-./cmake-build-debug/filesystemUtils
+./cmake-build-debug/fileSystemUtils
+./cmake-build-debug/filePathUtils
 ./cmake-build-debug/hashUtils
 ./cmake-build-debug/numberUtils
 ./cmake-build-debug/osMemoryUtils
 ./cmake-build-debug/stringUtils
+./cmake-build-debug/vfxTimeUtils
